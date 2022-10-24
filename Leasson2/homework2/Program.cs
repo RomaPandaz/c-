760c391239bce7ffe15946840c6cae3604316d48
student_1 <student@qwerty.com>
2022-10-24 11:55:15 +0300

    добавил альтернативный способ выовда во втором задании второй домашки, более правлиьный

--- Leasson2/homework2/Program.cs
+++ Leasson2/homework2/Program.cs
@@ -7,6 +7,8 @@ Random rnd = new Random();
 int value = rnd.Next(100,1000);
 int num = value;
 int num1 = num / 100;
-double num2 = Convert.ToDouble(num);
+int num2 = Convert.ToInt32(num);
 num2 = num2 % 10;
-Console.Write($"{value} -> {num1}{num2}");
+Console.WriteLine($"{value} -> {num1}{num2}");
+int result = (num1 * 10 + num2);                //алтернативный способ вывода 
+Console.WriteLine($"{value} -> {result}");      //алтернативный способ вывода 
