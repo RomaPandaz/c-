8a91b369163d687166d436d7bbca46d354930433
student_1 <student@qwerty.com>
2022-11-17 14:45:20 +0300

    прорешал 6ую домашку

---
+++ Leasson6/homework1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Пример: 0, 7, 8, -2, -2 -> 2
+//        -1, -7, 567, 89, 223-> 3
+
+int DigitCount(string message) {
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[] WriteArr(int lenArr) {
+    int[] tempArray = new int[lenArr];
+    for (int i = 0; i < lenArr; i++)
+    {
+        tempArray[i] = DigitCount("Введите число -> ");
+    }
+    return tempArray;
+}
+
+void printArray(int[] intArray){
+    int countDigit = 0;
+    for (int i = 0; i < intArray.Length; i++){
+        if (intArray[i] > 0)
+        {
+            countDigit++;
+        }
+        System.Console.Write($", {intArray[i]}");
+    }
+    Console.Write($" -> {countDigit}");
+}
+
+int count = DigitCount("Введите количество вводимых чисел -> ");
+int[] getArray = WriteArr(count);
+printArray(getArray);
+
---
+++ Leasson6/homework2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// Пример:
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// k1 * x + b1 = k2 * x + b2
+// k1 * x -k2 * x = b2 - b1
+// x = b2 - b1 / k1-k2
+ 
+double Promt(string message){
+   System.Console.Write(message);
+   return int.Parse(Console.ReadLine());
+}
+
+double b1 = Promt("Введите координату b1: ");
+double k1 = Promt("Введите координату k1: ");
+double b2 = Promt("Введите координату b2: ");
+double k2 = Promt("Введите координату k1: ");
+
+double x = (b2 - b1) / (k1-k2);
+double y = k2 * x + b2;
+
+Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
