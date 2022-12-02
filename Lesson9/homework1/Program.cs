bf606f91b22fb1244a6743aea2305763fb866c26
student_1 <student@qwerty.com>
2022-12-02 16:12:51 +0300

    9ая домашняя работв 2из3 заданий

---
+++ Lesson9/homework1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+string GetEven(int m, int n) {
+    if (m % 2 != 0 && m < n) m += 1;
+    if (m < 0) m = 2;
+    if (m > n) return "";
+    else return ($"{m}, " + GetEven(m + 2, n)).TrimEnd(' ', ','); 
+}
+
+void result() {
+    int m = 15;
+    int n = 30;
+    System.Console.WriteLine(GetEven(m, n));
+}
+
+result();
---
+++ Lesson9/homework2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Задайте значения M и N.
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int sum(int m, int n) {
+    if (m < 0) m = 1; 
+    if (m > n) return 0;
+    else return (m + sum(m + 1, n));
+}
+
+void result() {
+    int m = 15;
+    int n = 30;
+    System.Console.WriteLine(sum(m, n));
+}
+
+result();
